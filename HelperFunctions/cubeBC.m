d449f13ecd564658b900c83080e2d99318fd89bb
lijas <elias.borjesson@hotmail.com>
2016-02-19 16:14:26 +0100

    Error where bc:s were added wrongly

--- HelperFunctions/cubeBC.m
+++ HelperFunctions/cubeBC.m
@@ -6,14 +6,14 @@ function [f, bc] = cubeBC( opt, f, P, dof, side1nodes, side2nodes, side3nodes, s
 
 if strcmp('Konsol', opt)
     side2zdof = dof(side2nodes,3);
-    f(side2zdof) = P/length(side2zdof);
+    f(side2zdof) = f(side2zdof)+ P/length(side2zdof);
 
     ldofs = dof(side1nodes,:);
     ldofs = ldofs(:);
     bc = [ldofs,ldofs*0];
 elseif strcmp('KonsolMedUtbredd', opt)
     side5zdof = dof(side5nodes,3);
-    f(side5zdof) = P/length(side5zdof);
+    f(side5zdof) = f(side5zdof) + P/length(side5zdof);
 
     ldofs = dof(side1nodes,:);
     ldofs = ldofs(:);
@@ -29,7 +29,7 @@ elseif(strcmp('FrittUpplagt', opt))
     bc = [[ldofs;ldofs2],[ldofs;ldofs2]*0];
 elseif(strcmp('InspandPlatta', opt))
     side5zdof = dof(side5nodes,3);
-    f(side5zdof) = P/length(side5zdof);
+    f(side5zdof) = f(side5zdof) +  P/length(side5zdof);
     
     ldofs = dof([side1nodes;side2nodes;side3nodes;side4nodes], 1:3);
     ldofs = ldofs(:);
